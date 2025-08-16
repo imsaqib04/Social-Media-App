a5322dadf30e9bd5db8fb3db8c5fdcc3407a91f8
saqib <saqibansari2011@gmail.com>
2025-08-16 10:57:24 +0530

    user done

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import UserEditForm ,ProfileEditForm
 # Create your views here.
 
 def user_login(request):
@@ -26,3 +28,83 @@ def user_login(request):
 @login_required
 def index(request):
     return render (request,'users/index.html')
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render (request,'users/register_done.html')
+    else:
+        user_form = UserRegistrationForm()
+    return render (request,'users/register.html',{'user_form':user_form})
+
+
+# @login_required
+# def edit(request):
+#     if request.method=="POST":
+#         user_form = UserEditForm(instance=request.user,data=request.POST)
+#         profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+        
+#     else:
+#         user_form = UserEditForm(instance=request.user)
+#         profile_form = ProfileEditForm(instance=request.user.profile)
+
+#         return render(request,'users/edit.html',{'user_form':user_form,'profile_form':profile_form})
+
+# @login_required
+# def edit(request):
+#     # Ensure profile exists for current user
+#     profile, created = Profile.objects.get_or_create(user=request.user)
+
+#     if request.method == "POST":
+#         user_form = UserEditForm(instance=request.user, data=request.POST)
+#         profile_form = ProfileEditForm(instance=profile, data=request.POST, files=request.FILES)
+
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+#             return render(request, 'users/edit.html', {
+#                 'user_form': user_form,
+#                 'profile_form': profile_form,
+#                 'success': True
+#             })
+#     else:
+#         user_form = UserEditForm(instance=request.user)
+#         profile_form = ProfileEditForm(instance=profile)
+
+#     # Always return a response
+#     return render(request, 'users/edit.html', {
+#         'user_form': user_form,
+#         'profile_form': profile_form
+#     })
+
+@login_required
+def edit(request):
+    profile, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('edit')  # <--- redirect after success
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=profile)
+
+    return render(request, 'users/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
