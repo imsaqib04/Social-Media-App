90f05b69c8de22c1b9183d07c53ad9edf8f179b3
saqib <saqibansari2011@gmail.com>
2025-08-17 13:12:29 +0530

    changes

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users",
+    "posts",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -122,6 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 LOGIN_URL='login'
 LOGOUT_URL='logout'
 LOGOUT_REDIRECT_URL = "login"
